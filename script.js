8097b141e32070ff36e5d36f5cf1d849a4916f78
Kacper Czechowicz <czechowicz.kacper@gmail.com>
2015-06-10 16:28:46 +0200

    script

---
+++ script.js
@@ -0,0 +1,26 @@
+(function($){
+
+	var stickyHeader = function(){
+		var $doc = $(document),
+			$header = $('.menu'),
+			$headerWrapper = $('.menu-wrapper'),
+			fixedClass = 'menu-fixed';
+
+			if(!$header.hasClass(fixedClass) && $doc.scrollTop() > $headerWrapper.position().top){
+				$headerWrapper.height($header.height());
+				$header.addClass(fixedClass);
+			}else if($header.hasClass(fixedClass) && $doc.scrollTop() <= $headerWrapper.position().top){
+				$headerWrapper.height($header.height());
+				$header.removeClass(fixedClass);
+			}
+	};
+
+
+	$(document).ready(function(){
+		stickyHeader();
+
+		$(window).scroll(stickyHeader);
+	});
+})(jQuery);
+
+
