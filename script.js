5f609c6b6a779f94a8bad46b446f9f19a1d60baf
Kacper Czechowicz <czechowicz.kacper@gmail.com>
2015-06-15 20:50:17 +0200

    HELL YEA NEW SITE

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-(function($){
-
-	var stickyHeader = function(){
-		var $doc = $(document),
-			$header = $('.menu'),
-			$headerWrapper = $('.menu-wrapper'),
-			fixedClass = 'menu-fixed';
-
-			if(!$header.hasClass(fixedClass) && $doc.scrollTop() > $headerWrapper.position().top){
-				$headerWrapper.css('min-height', $header.height()+1);
-				$header.addClass(fixedClass);
-			}else if($header.hasClass(fixedClass) && $doc.scrollTop() <= $headerWrapper.position().top){
-				$header.removeClass(fixedClass);
-			}
-	};
-
-
-	$(document).ready(function(){
-		stickyHeader();
-
-		$(window).scroll(stickyHeader);
-	});
-})(jQuery);
-
-
+//(function($){
+//
+//	var stickyHeader = function(){
+//		var $doc = $(document),
+//			$header = $('.menu'),
+//			$headerWrapper = $('.menu-wrapper'),
+//			fixedClass = 'menu-fixed';
+//
+//			if(!$header.hasClass(fixedClass) && $doc.scrollTop() > $headerWrapper.position().top){
+//				$headerWrapper.css('min-height', $header.height()+1);
+//				$header.addClass(fixedClass);
+//			}else if($header.hasClass(fixedClass) && $doc.scrollTop() <= $headerWrapper.position().top){
+//				$header.removeClass(fixedClass);
+//			}
+//	};
+//
+//
+//	$(document).ready(function(){
+//		stickyHeader();
+//
+//		$(window).scroll(stickyHeader);
+//	});
+//})(jQuery);
+//
+//
